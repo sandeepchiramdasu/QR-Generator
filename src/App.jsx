46c1d0cf38d1ce0a37722a0b7a6ccc00f08133ac
chiradasusandeep <chiramdasusandeep@gmail.com>
2025-08-31 09:38:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import QRGenerator from './components/QRGenerator'
+import Calculator from './components/Calculator'
+
+const App = () => {
+  return (
+    <div>
+      <QRGenerator />
+      {/* <Calculator /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/QRGenerator.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import QRCode from 'react-qr-code'
+import './QRGenerator.css'
+
+
+const QRGenerator = () => {
+  const [qrcode, setQrcode] = useState('')
+  const [input, setInput] = useState('')
+  const [format, setFormat] = useState('png')
+
+  const handleClick = () => {
+    if (input.trim() === '') {
+      alert('Please enter text...')
+    } else {
+      setQrcode(input)
+    }
+  }
+
+  const handleDownload = () => {
+    if (!qrcode)
+        alert('please generate QR')
+        return
+    const svg = document.getElementById('qr-code-generator')
+    const svgData = new XMLSerializer().serializeToString(svg)
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    const img = new Image()
+    img.src = 'data:image/svg+xml;base64,' + btoa(svgData)
+    img.onload = () => {
+      canvas.width = img.width
+      canvas.height = img.height
+      ctx.drawImage(img, 0, 0)
+      const link = document.createElement('a')
+      link.href = canvas.toDataURL(`image/${format}`)
+      link.download = `qrcode.${format}`
+      link.click()
+    }
+  }
+
+  return (
+    <div className="qr-container">
+        
+      
+      <div className="qr-left">
+        <h1>QR Code Generator</h1>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter text here"
+          className="qr-input"
+        />
+        <button onClick={handleClick} className="qr-btn">
+          Generate
+        </button>
+      </div>
+
+      
+      <div className="qr-right">
+        <QRCode id="qr-code-generator" className='qr' value={qrcode} size={400} />
+        <div className="qr-options">
+          <select  className='format-selection' value={format} onChange={(e) => setFormat(e.target.value)}>
+            <option value="png">PNG</option>
+            <option value="jpg">JPG</option>
+          </select>
+          <button onClick={handleDownload} className="qr-btn">
+            Download QR
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default QRGenerator
